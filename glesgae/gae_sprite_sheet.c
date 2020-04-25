8ee852b0b1a5a51eebd7a6c07e4f5823f3e405f7
Steven Campbell <stuckie@arcadebadgers.co.uk>
2020-04-25 18:45:56 +0100

    Added animation support to gae sprite sheet Takes animations as an array of frames defined as sprites in the json

--- glesgae/gae_sprite_sheet.c
+++ glesgae/gae_sprite_sheet.c
@@ -16,6 +16,12 @@ typedef struct sprite_node_s {
 	gae_rect_t srcRect;
 } sprite_node_t;
 
+/* Key (name) : Value (array) */
+typedef struct anim_node_s {
+	gae_hashstring name;
+	gae_array_t frames;
+} anim_node_t;
+
 static void loadTexture(gae_graphics_texture_t* texture, gae_json_document_t* jsDoc, gae_json_node_t* node)
 {
 	char* value = gae_json_string(jsDoc, &node->value);
@@ -47,20 +53,49 @@ static sprite_node_t parseSprite(gae_json_document_t* jsDoc, gae_json_node_t* no
 	return sprite;
 }
 
+static anim_node_t parseAnim(gae_json_document_t* jsDoc, gae_json_node_t* node)
+{
+	anim_node_t anim;
+	char* key = gae_json_string(jsDoc, &node->name);
+	gae_json_node_t* animNode = node->child;
+
+	anim.name = gae_hashstring_calculate(key);
+	gae_array_init(&anim.frames, sizeof(gae_hashstring));
+    
+	while (0 != animNode) {
+		char* name = gae_json_string(jsDoc, &animNode->name);
+		gae_hashstring hash = gae_hashstring_calculate(name);
+		gae_array_push(&anim.frames, &hash);
+		animNode = animNode->sibling;
+	}
+
+	return anim;
+}
+
 gae_sprite_sheet_t* gae_sprite_sheet_init(gae_sprite_sheet_t* sheet, gae_json_document_t* jsDoc)
 {
 	gae_json_node_t* filename = gae_json_node_find(jsDoc, jsDoc->root, "filename");
 	gae_json_node_t* sprites = gae_json_node_find(jsDoc, jsDoc->root, "sprites");
-	gae_json_node_t* sprite = sprites->child;
+	gae_json_node_t* anims = gae_json_node_find(jsDoc, jsDoc->root, "animations");
+	gae_json_node_t* sprite = (0 != sprites) ? sprites->child : 0;
+	gae_json_node_t* anim = (0 != anims) ? anims->child : 0;
 	
-	gae_map_init(&sheet->data, sizeof(gae_hashstring), sizeof(gae_rect_t), gae_hashstring_compare);
+	gae_map_init(&sheet->sprites, sizeof(gae_hashstring), sizeof(gae_rect_t), gae_hashstring_compare);
+    gae_map_init(&sheet->animations, sizeof(gae_hashstring), sizeof(gae_array_t), gae_hashstring_compare);
+    
 	gae_graphics_texture_init(&sheet->texture);
 	if (0 != filename) loadTexture(&sheet->texture, jsDoc, filename);
 	
 	while (0 != sprite) { /* sprites is an array of objects of arrays.. sprite = obj, sprite->child = inner array */
 		sprite_node_t newSprite = parseSprite(jsDoc, sprite->child);
-		gae_map_push(&sheet->data, &newSprite.name, &newSprite.srcRect);
+		gae_map_push(&sheet->sprites, &newSprite.name, &newSprite.srcRect);
 		sprite = sprite->sibling;
+	}
+
+	while (0 != anim) { /* anims is an array of objects of arrays.. anim = obj, anim->child = inner array */ 
+		anim_node_t newAnim = parseAnim(jsDoc, anim->child);
+		gae_map_push(&sheet->animations, &newAnim.name, &newAnim.frames);
+		anim = anim->sibling;
 	}
 	
 	return sheet;
@@ -68,12 +103,17 @@ gae_sprite_sheet_t* gae_sprite_sheet_init(gae_sprite_sheet_t* sheet, gae_json_do
 
 gae_rect_t* gae_sprite_sheet_get_rect(gae_sprite_sheet_t* const sheet, gae_hashstring sprite)
 {
-	return gae_map_get(&sheet->data, &sprite);
+	return gae_map_get(&sheet->sprites, &sprite);
+}
+
+gae_array_t* gae_sprite_sheet_get_animation_frames(gae_sprite_sheet_t* const sheet, gae_hashstring animation)
+{
+	return gae_map_get(&sheet->animations, &animation);
 }
 
 gae_sprite_sheet_t* gae_sprite_sheet_draw(gae_sprite_sheet_t* const sheet, gae_hashstring sprite, gae_rect_t* const pos)
 {
-	gae_rect_t* drawRect = gae_map_get(&sheet->data, &sprite);
+	gae_rect_t* drawRect = gae_map_get(&sheet->sprites, &sprite);
 	if (0 == drawRect) return sheet;
 
 	pos->w = drawRect->w;
@@ -85,7 +125,8 @@ gae_sprite_sheet_t* gae_sprite_sheet_draw(gae_sprite_sheet_t* const sheet, gae_h
 
 gae_sprite_sheet_t* gae_sprite_sheet_destroy(gae_sprite_sheet_t* sheet)
 {
-	gae_map_destroy(&sheet->data);
+	gae_map_destroy(&sheet->sprites);
+    gae_map_destroy(&sheet->animations);
 	gae_graphics_texture_destroy(&sheet->texture);
 	
 	return sheet;
--- glesgae/gae_sprite_sheet.h
+++ glesgae/gae_sprite_sheet.h
@@ -5,20 +5,29 @@
 #include "gae_map.h"
 #include "gae_hashstring.h"
 
+struct gae_array_s;
 struct gae_json_document_s;
 struct gae_rect_s;
 
 typedef struct gae_sprite_sheet_s {
 	gae_graphics_texture_t texture;
-	gae_map_t data;
+	gae_map_t sprites;
+    gae_map_t animations;
 } gae_sprite_sheet_t;
 
+/* Initialise a sprite sheet from the given json document */
 gae_sprite_sheet_t* gae_sprite_sheet_init(gae_sprite_sheet_t* sheet, struct gae_json_document_s* jsDoc);
 
+/* Helper function to draw a single sprite */
 gae_sprite_sheet_t* gae_sprite_sheet_draw(gae_sprite_sheet_t* const sheet, gae_hashstring sprite, struct gae_rect_s* const pos);
 
+/* Get the rect description of a sprite */
 struct gae_rect_s* gae_sprite_sheet_get_rect(gae_sprite_sheet_t* const sheet, gae_hashstring sprite);
 
+/* Get an array of sprites from this sprite sheet that make up an animation */
+struct gae_array_s* gae_sprite_sheet_get_animation_frames(gae_sprite_sheet_t* const sheet, gae_hashstring animation);
+
+/* Destroy the sprite sheet and clean up any memory created by it */
 gae_sprite_sheet_t* gae_sprite_sheet_destroy(gae_sprite_sheet_t* sheet);
 
 #endif
