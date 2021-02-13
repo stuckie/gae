3f20310b094f9ef36ca2c83daef678c89ced9307
Steven Campbell <stuckie@arcadebadgers.co.uk>
2021-02-13 21:01:58 +0000

    Added osxcross Makefile Small fix for gae_system as osxcross didn't like it in the .c

--- glesgae/gae_system.h
+++ glesgae/gae_system.h
@@ -19,7 +19,7 @@ typedef struct gae_system_s {
 	struct gae_clock_s* main_clock;
 } gae_system_t;
 
-extern gae_system_t gae_system;
+gae_system_t gae_system;
 
 void gae_system_delay(unsigned int ms);
 
