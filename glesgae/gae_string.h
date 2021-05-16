4c2c8d72a8a665c39e3baa8bf1af5e5b1596f0f8
Steven Campbell <stuckie@arcadebadgers.co.uk>
2021-05-16 10:45:49 +0100

    Update macro for gae_sprintf

--- glesgae/gae_string.h
+++ glesgae/gae_string.h
@@ -1,9 +1,9 @@
 #ifndef _GAE_STRING_H_
 #define _GAE_STRING_H_
 
-#if defined(LINUX)
+#if !defined(WINDOWS)
 	#define gae_sprintf(dst, size, format, ...) sprintf((char*)dst, format, __VA_ARGS__);
-#elif defined(WINDOWS)
+#else
 	#define gae_sprintf(dst, size, format, ...) sprintf_s((char* const)dst, size, format, __VA_ARGS__);
 #endif
 
