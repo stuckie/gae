893bf0919fc50e5be8f824eb692e009d404f973d
Steven Campbell <stuckie@arcadebadgers.co.uk>
2021-05-16 10:58:02 +0100

    *mumble* *grumble* MSVC vs MINGW

--- glesgae/gae_string.h
+++ glesgae/gae_string.h
@@ -1,7 +1,7 @@
 #ifndef _GAE_STRING_H_
 #define _GAE_STRING_H_
 
-#if !defined(WINDOWS)
+#if !defined(MSVC)
 	#define gae_sprintf(dst, size, format, ...) sprintf((char*)dst, format, __VA_ARGS__);
 #else
 	#define gae_sprintf(dst, size, format, ...) sprintf_s((char* const)dst, size, format, __VA_ARGS__);
